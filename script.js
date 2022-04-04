0aa4f035a1843918b86fc7498b97213595257ec5
Andrew <asjogren@leisdstudent.ws>
2022-04-04 10:57:12 -0500

    second

---
+++ script.js
@@ -0,0 +1,7 @@
+function run() {
+    document.getElementById("paragraph").innerHTML = "Hello World!";
+    document.getElementById("paragraph").style.backgroundColor = "green";
+    document.getElementById("paragraph").style.color = "white";
+    document.getElementById("paragraph").style.padding = "20px";
+}
+
